010c3b407504f18c1c43e2d75bd94845d86656c7
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2023-03-24 12:59:15 -0400

    Add room page

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,31 +2,32 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
+import Room from "./Room";
 import {
-    BrowserRouter as Router, 
-    Switch, 
-    Link, 
-    Route, 
-    Redirect 
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route,
+  Redirect,
 } from "react-router-dom";
 
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default class HomePage extends Component{
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <p>This is a home page!</p>
-                    </Route>
-                    <Route path="/join" component={RoomJoinPage} />
-                    <Route path='/create' component={CreateRoomPage} />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is a home page!</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/Room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }

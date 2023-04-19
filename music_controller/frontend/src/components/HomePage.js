d4a791fba49d2d0fd210f89b95877bea21af9f80
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2023-04-19 01:23:25 -0400

    Add info component

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Info from "./info";
 
 import Room from "./Room";
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Switch,
-  
   Route,
   Link,
   Redirect,
@@ -45,6 +45,9 @@ export default class HomePage extends Component {
             <Button color="primary" to="/join" component={Link}>
               Join a Room
             </Button>
+            <Button color="default" to="/info" component={Link}>
+              Info
+            </Button>
             <Button color="secondary" to="/create" component={Link}>
               Create a Room
             </Button>
@@ -76,6 +79,7 @@ export default class HomePage extends Component {
             }}
           />
           <Route path="/join" component={RoomJoinPage} />
+          <Route path="/info" component={Info} />
           <Route path="/create" component={CreateRoomPage} />
           <Route
             path="/room/:roomCode"

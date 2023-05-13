ec51d904663c35800ae6896acc77828f8ca147a8
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2023-05-13 17:22:31 -0400

    use functional component

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,20 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
 
-export default class App extends Component{
-    constructor(props){
-        super(props);
-    }
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-    render() {
-        return (
-        <div className="center">
-            <HomePage />
-        </div>
-        );
-    }
-}
+//   render() {
+//     return (
+//       <div className="center">
+//         <HomePage />
+//       </div>
+//     );
+//   }
+// }
+
+export default function App () {
+  return (
+    <div className="center">
+      <HomePage />
+    </div>
+  );
+};
 
 const appDiv = document.getElementById("app");
-render(<App/>, appDiv);
+render(<App />, appDiv);
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Info from "./info";
@@ -13,26 +13,25 @@ import {
   Redirect,
 } from "react-router-dom";
 
-export default class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      roomCode: null,
-    };
-    this.clearRoomCode = this.clearRoomCode.bind(this);
-  }
+export default function HomePage(props) {
+  const [roomCode, setRoomCode] = useState(null);
 
-  async componentDidMount() {
-    fetch("/api/user-in-room")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          roomCode: data.code,
+  useEffect(() => {
+    async function getRoomCode() {
+      fetch("/api/user-in-room")
+        .then((response) => {
+          response.json();
+          console.log(response);
+        })
+        .then((data) => {
+          console.log(data);
+          setRoomCode(data.code);
         });
-      });
-  }
+    }
+    getRoomCode();
+  });
 
-  renderHomePage() {
+  function renderHomePage() {
     return (
       <Grid container spacing={3}>
         <Grid item xs={12} align="center">
@@ -57,38 +56,110 @@ export default class HomePage extends Component {
     );
   }
 
-  clearRoomCode() {
-    this.setState({
-      roomCode: null,
-    });
-  }
-
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => {
-              return this.state.roomCode ? (
-                <Redirect to={`/room/${this.state.roomCode}`} />
-              ) : (
-                this.renderHomePage()
-              );
-            }}
-          />
-          <Route path="/join" component={RoomJoinPage} />
-          <Route path="/info" component={Info} />
-          <Route path="/create" component={CreateRoomPage} />
-          <Route
-            path="/room/:roomCode"
-            render={(props) => {
-              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
-            }}
-          />
-        </Switch>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return roomCode ? (
+              <Redirect to={`/room/${roomCode}`} />
+            ) : (
+              renderHomePage()
+            );
+          }}
+        />
+        <Route path="/join" component={RoomJoinPage} />
+        <Route path="/info" component={Info} />
+        <Route path="/create" component={CreateRoomPage} />
+        <Route
+          path="/room/:roomCode"
+          render={(props) => {
+            return <Room {...props} leaveRoomCallback={setRoomCode(null)} />;
+          }}
+        />
+      </Switch>
+    </Router>
+  );
 }
+
+// export default class HomePage extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       roomCode: null,
+//     };
+//     this.clearRoomCode = this.clearRoomCode.bind(this);
+//   }
+
+//   async componentDidMount() {
+//     fetch("/api/user-in-room")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         this.setState({
+//           roomCode: data.code,
+//         });
+//       });
+//   }
+
+//   renderHomePage() {
+//     return (
+//       <Grid container spacing={3}>
+//         <Grid item xs={12} align="center">
+//           <Typography variant="h3" compact="h3">
+//             House Party
+//           </Typography>
+//         </Grid>
+//         <Grid item xs={12} align="center">
+//           <ButtonGroup disableElevation variant="contained" color="primary">
+//             <Button color="primary" to="/join" component={Link}>
+//               Join a Room
+//             </Button>
+//             <Button color="default" to="/info" component={Link}>
+//               Info
+//             </Button>
+//             <Button color="secondary" to="/create" component={Link}>
+//               Create a Room
+//             </Button>
+//           </ButtonGroup>
+//         </Grid>
+//       </Grid>
+//     );
+//   }
+
+//   clearRoomCode() {
+//     this.setState({
+//       roomCode: null,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <Router>
+//         <Switch>
+//           <Route
+//             exact
+//             path="/"
+//             render={() => {
+//               return this.state.roomCode ? (
+//                 <Redirect to={`/room/${this.state.roomCode}`} />
+//               ) : (
+//                 this.renderHomePage()
+//               );
+//             }}
+//           />
+//           <Route path="/join" component={RoomJoinPage} />
+//           <Route path="/info" component={Info} />
+//           <Route path="/create" component={CreateRoomPage} />
+//           <Route
+//             path="/room/:roomCode"
+//             render={(props) => {
+//               return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+//             }}
+//           />
+//         </Switch>
+//       </Router>
+//     );
+//   }
+// }

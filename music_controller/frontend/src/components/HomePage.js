076130f06ddd6c49404ab4dec2ab7a5a3e664935
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2023-06-05 21:10:52 -0400

    use functional component

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Info from "./info";
@@ -52,6 +52,10 @@ export default function HomePage(props) {
     );
   }
 
+  function clearRoomCode() {
+    setRoomCode(null);
+  }
+
   return (
     <Router>
       <Switch>
@@ -72,7 +76,7 @@ export default function HomePage(props) {
         <Route
           path="/room/:roomCode"
           render={(props) => {
-            return <Room {...props} leaveRoomCallback={setRoomCode(null)} />;
+            return <Room {...props} leaveRoomCallback={clearRoomCode} />;
           }}
         />
       </Switch>
@@ -80,82 +84,3 @@ export default function HomePage(props) {
   );
 }
 
-// export default class HomePage extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       roomCode: null,
-//     };
-//     this.clearRoomCode = this.clearRoomCode.bind(this);
-//   }
-
-//   async componentDidMount() {
-//     fetch("/api/user-in-room")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         this.setState({
-//           roomCode: data.code,
-//         });
-//       });
-//   }
-
-//   renderHomePage() {
-//     return (
-//       <Grid container spacing={3}>
-//         <Grid item xs={12} align="center">
-//           <Typography variant="h3" compact="h3">
-//             House Party
-//           </Typography>
-//         </Grid>
-//         <Grid item xs={12} align="center">
-//           <ButtonGroup disableElevation variant="contained" color="primary">
-//             <Button color="primary" to="/join" component={Link}>
-//               Join a Room
-//             </Button>
-//             <Button color="default" to="/info" component={Link}>
-//               Info
-//             </Button>
-//             <Button color="secondary" to="/create" component={Link}>
-//               Create a Room
-//             </Button>
-//           </ButtonGroup>
-//         </Grid>
-//       </Grid>
-//     );
-//   }
-
-//   clearRoomCode() {
-//     this.setState({
-//       roomCode: null,
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <Router>
-//         <Switch>
-//           <Route
-//             exact
-//             path="/"
-//             render={() => {
-//               return this.state.roomCode ? (
-//                 <Redirect to={`/room/${this.state.roomCode}`} />
-//               ) : (
-//                 this.renderHomePage()
-//               );
-//             }}
-//           />
-//           <Route path="/join" component={RoomJoinPage} />
-//           <Route path="/info" component={Info} />
-//           <Route path="/create" component={CreateRoomPage} />
-//           <Route
-//             path="/room/:roomCode"
-//             render={(props) => {
-//               return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
-//             }}
-//           />
-//         </Switch>
-//       </Router>
-//     );
-//   }
-// }
